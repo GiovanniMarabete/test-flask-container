1b84aa61e563ec5c0e94ab7900699143ea063274
Giovanni Marabete <gio@kimiki.solutions>
2022-05-19 09:50:12 +0100

    First Commit

---
+++ app/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Pippo',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'Febraury 20, 2019'
+    },
+    {
+        'author': 'Pluto',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'Febraury 28, 2019'
+    }
+
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title ='About')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title ='Login')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title ='Register')
+
+if __name__== '__main__':
+    app.run(debug=True, host='0.0.0.0', port="80")
+    
